60a9bbf938ad16c98a61b7c3958a88ced7daaa22
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-05-10 23:08:55 +0530

    Neetcode - Implement search in rotated sorted array

---
+++ Binary Search/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+            int m = (l + r) >> 1;
+
+            if (nums[m] == target)
+                return m;
+
+            // Left sorted part
+            if (nums[l] <= nums[m]) {
+                if (target >= nums[l] && target < nums[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            }
+            // Right sorted part
+            else {
+                if (target > nums[m] && target <= nums[r])
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+        }
+
+        return -1;
+    }
+};
