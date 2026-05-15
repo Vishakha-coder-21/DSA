c5956dab5fac6345b0e18d4a5a039f37d80d2f2a
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-05-15 20:56:23 +0530

    Add files via upload

---
+++ Day2/WordCapitlization1.cpp
@@ -0,0 +1,18 @@
+// Convert only 1st ch
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  char ch;
+  cin.get(ch);
+
+  if (ch >= 97 and ch <= 122) {
+    ch -= 32;
+  }
+  cout << ch;
+
+  while (cin.get(ch)) {
+    cout << ch;
+  }
+}
