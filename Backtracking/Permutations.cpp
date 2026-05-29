5df7aec6c4ee9fa5c535820daf46e42e0b070859
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-05-29 22:01:29 +0530

    Neetcode - Add backtracking solution for permutations
    Implement backtracking algorithm to generate all permutations of a given vector of integers.

---
+++ Backtracking/Permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    inline void solve(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); ++i) {
+            swap(nums[idx], nums[i]);
+
+            solve(nums, idx + 1);
+
+            swap(nums[idx], nums[i]); // backtrack
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        ans.reserve(720); // max 6! permutations
+        solve(nums, 0);
+        return ans;
+    }
+};
