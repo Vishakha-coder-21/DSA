5ae686e42c04b1531a2f65f93ba58dda8e1b588a
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-06-03 23:37:47 +0530

    Leetcode (3635) - Array + Greedy + Binary Search + Two pointer

---
+++ Two Pointer/Earliest Finish Time for Land and Water Rides II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+
+        const int n = landStartTime.size();
+        const int m = waterStartTime.size();
+
+        int minLandEnd = INT_MAX;
+        for (int i = 0; i < n; ++i)
+            minLandEnd = min(minLandEnd,
+                             landStartTime[i] + landDuration[i]);
+
+        int minWaterEnd = INT_MAX;
+        for (int i = 0; i < m; ++i)
+            minWaterEnd = min(minWaterEnd,
+                              waterStartTime[i] + waterDuration[i]);
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < m; ++i)
+            ans = min(ans,
+                      max(minLandEnd, waterStartTime[i]) +
+                      waterDuration[i]);
+
+        for (int i = 0; i < n; ++i)
+            ans = min(ans,
+                      max(minWaterEnd, landStartTime[i]) +
+                      landDuration[i]);
+
+        return ans;
+    }
+};
