d93f1df59e690b8c217bf91d19872c3c1255b66f
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-06-12 22:27:18 +0530

    Neetcode

---
+++ Recursion/Pow(x, n).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        if (N == 0) return 1;
+
+        double half = myPow(x, N / 2);
+
+        return N % 2 ? half * half * x : half * half;
+    }
+};
