d0c6481b6598e5f507544e3f2a6c5522279ccca0
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-06-30 22:07:54 +0530

    Leetcode 1358 -  String + Sliding Window +Hash table

---
+++ Sliding Window/Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int last[3] = {-1, -1, -1};
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+            ans += min(last[0], min(last[1], last[2])) + 1;
+        }
+        return ans;
+    }
+};
