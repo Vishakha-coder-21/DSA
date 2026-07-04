777ca04813ba7a839481ea42ae6585b9b6e6bf79
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-07-04 22:39:09 +0530

    Leetcode 225 - Stack + Queue + Design

---
+++ Queue/Implement Stack using Queues.cpp
@@ -0,0 +1,30 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {}
+
+    void push(int x) {
+        int sz = q.size();
+        q.push(x);
+
+        while (sz--) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
