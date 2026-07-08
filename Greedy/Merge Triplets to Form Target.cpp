905a052964e28ef714424585e1b5d517e713e216
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-07-08 23:14:05 +0530

    Neetcode - Array + Greedy

---
+++ Greedy/Merge Triplets to Form Target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        bool x = false, y = false, z = false;
+
+        for (const auto &t : triplets) {
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2])
+                continue;
+
+            if (t[0] == target[0]) x = true;
+            if (t[1] == target[1]) y = true;
+            if (t[2] == target[2]) z = true;
+
+            if (x && y && z) return true;
+        }
+
+        return false;
+    }
+};
