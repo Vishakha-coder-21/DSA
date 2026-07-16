19fb9ed657407281386cbdbe15392c591702a498
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-07-16 23:14:40 +0530

    Leetcode 3867 - Array + Sorting + Math + Num Theory

---
+++ Two Pointer/Sum of GCD of Formed Pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        vector<int> a;
+        int mx = 0;
+        for (int x : nums) {
+            mx = max(mx, x);
+            a.push_back(gcd(x, mx));
+        }
+        sort(a.begin(), a.end());
+
+        long long ans = 0;
+        for (int l = 0, r = a.size() - 1; l < r; ++l, --r)
+            ans += gcd(a[l], a[r]);
+
+        return ans;
+    }
+};
