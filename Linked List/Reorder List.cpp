589f4e8aab2f40762d8bb5701ed6dd038662ec0d
Vishakha Sharda <vishakha.coder21@gmail.com>
2026-07-21 22:02:12 +0530

    Leetcode 143 - Two Pointer + Recursion + Stack

---
+++ Linked List/Reorder List.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        // Find middle
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode *prev = nullptr, *curr = slow->next;
+        slow->next = nullptr;
+
+        while (curr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Merge two halves
+        ListNode *first = head, *second = prev;
+        while (second) {
+            ListNode *t1 = first->next;
+            ListNode *t2 = second->next;
+
+            first->next = second;
+            second->next = t1;
+
+            first = t1;
+            second = t2;
+        }
+    }
+};
